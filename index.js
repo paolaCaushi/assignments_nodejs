9727c426f28f0dc260a128497561875ba57e09a0
paolaCaushi <pcaushi@unitsystem.net>
2022-06-15 09:16:47 +0200

    fixed error

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require('./app/calc');
-require('./app/concat');
-require('./app/index');
+const { sum } = require('./app/calc');
+const { concat } = require('./app/concat');
+const { readFileAsync } = require('./app/index');
 
 
 readFileAsync(".data.json").then(data=> {
     console.log(`The result of the sum is ${sum(data)}`);
     console.log(`The result of the concat is ${concat(data)}`);
 });
+
